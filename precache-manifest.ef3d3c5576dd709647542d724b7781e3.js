0114521ab576b3509ac3bab1cfe9cdc2332411bc
jonusis <jonusis@github.com>
2019-05-20 01:08:49 +0800

    Updates

---
+++ precache-manifest.ef3d3c5576dd709647542d724b7781e3.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = [
+  {
+    "revision": "e5cd2ba26c20796b4784adf9fd795b20",
+    "url": "/eaty_snake/static/media/button_start.e5cd2ba2.png"
+  },
+  {
+    "revision": "7fead4c001fdb0765bbd",
+    "url": "/eaty_snake/static/css/main.6610ca9a.chunk.css"
+  },
+  {
+    "revision": "419b6e754f06dd0b99a5",
+    "url": "/eaty_snake/static/js/runtime~main.419b6e75.js"
+  },
+  {
+    "revision": "a976778d8fd126921a02",
+    "url": "/eaty_snake/static/js/2.a976778d.chunk.js"
+  },
+  {
+    "revision": "ab56a23ea308c5e947199e9c20b3e6bd",
+    "url": "/eaty_snake/static/media/button.ab56a23e.png"
+  },
+  {
+    "revision": "91261481bfae971ca758e1987ab12422",
+    "url": "/eaty_snake/static/media/snake_logo.91261481.png"
+  },
+  {
+    "revision": "7fead4c001fdb0765bbd",
+    "url": "/eaty_snake/static/js/main.7fead4c0.chunk.js"
+  },
+  {
+    "revision": "c9826a8c7966fd9a35bc8c06b6547439",
+    "url": "/eaty_snake/static/media/description.c9826a8c.png"
+  },
+  {
+    "revision": "73ea8a9b344f9f408fe3c7778fbf31c0",
+    "url": "/eaty_snake/static/media/move_snake.73ea8a9b.png"
+  },
+  {
+    "revision": "2e49753106b0cb3103aa308abc755a48",
+    "url": "/eaty_snake/static/media/small_snake.2e497531.png"
+  },
+  {
+    "revision": "71672783544b2525e30566d0c7688b79",
+    "url": "/eaty_snake/static/media/losesnake.71672783.png"
+  },
+  {
+    "revision": "aad965842dc4cb57ef8334cb65734dfb",
+    "url": "/eaty_snake/static/media/winsnake.aad96584.png"
+  },
+  {
+    "revision": "970f953fe8c8940de26767eb3490a3aa",
+    "url": "/eaty_snake/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.f7757aa413c424a484a0f29b674fbf1d.js"
+  "/eaty_snake/precache-manifest.ef3d3c5576dd709647542d724b7781e3.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("./index.html", {
+workbox.routing.registerNavigationRoute("/eaty_snake/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
